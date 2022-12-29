e5b6df57ae91066a21056e82a012e67a250aff11
Daniel Howard <acrodemocide@hotmail.com>
2022-12-29 16:42:03 -0700

    Updated requirements and views. Added file for processing stock analysis

--- StockAnalyzerApi/StockAnalyzer/views.py
+++ StockAnalyzerApi/StockAnalyzer/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the StockAnalyzer index.")
+    return HttpResponse("Hello, world. You're at the StockAnalyzer index. More Stuff")
