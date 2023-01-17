ab54a97e7acda482736119b9d461e9264383f523
Daniel Howard <acrodemocide@hotmail.com>
2023-01-17 15:47:12 -0700

    Updated class to use only generics for our API endpoints

--- StockAnalyzer/Analyzer/views.py
+++ StockAnalyzer/Analyzer/views.py
@@ -1,41 +1,15 @@
-# from rest_framework import status
-# from django.http import Http404
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
 from Analyzer.models import Stock
 from Analyzer.serializers import StockSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 # Create your views here.
-class StockList(mixins.ListModelMixin,
-                mixins.CreateModelMixin,
-                generics.GenericAPIView):
-
-  queryset = Stock.objects.all()
-  serializer_class = StockSerializer
-  
-  def get(self, request, *args, **kwargs):
-    return self.list(request, *args, **kwargs)
-
-  def post(self, request, *args, **kwargs):
-    return self.create(request, *args, **kwargs)
-        
-
-class StockDetail(mixins.RetrieveModelMixin,
-                  mixins.UpdateModelMixin,
-                  mixins.DestroyModelMixin,
-                  generics.GenericAPIView):
+class StockList(generics.ListCreateAPIView):
 
   queryset = Stock.objects.all()
   serializer_class = StockSerializer
 
-  def get(self, request, *args, **kwargs):
-    return self.retrieve(request, *args, **kwargs)
+class StockDetail(generics.RetrieveUpdateDestroyAPIView):
 
-  def put(self, request, *args, **kwargs):
-    return self.update(request, *args, **kwargs)
-    
-  def delete(self, request, *args, **kwargs):
-    return self.destroy(request, *args, **kwargs)
+  queryset = Stock.objects.all()
+  serializer_class = StockSerializer
     
