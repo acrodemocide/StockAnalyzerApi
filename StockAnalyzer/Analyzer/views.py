cb10ceb81b91c2da3316425e56605df8fe6bb445
Daniel Howard <acrodemocide@hotmail.com>
2023-01-17 15:41:57 -0700

    Updated to use mixins

--- StockAnalyzer/Analyzer/views.py
+++ StockAnalyzer/Analyzer/views.py
@@ -1,54 +1,41 @@
-from rest_framework import status
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
+# from rest_framework import status
+# from django.http import Http404
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
 from Analyzer.models import Stock
 from Analyzer.serializers import StockSerializer
-
+from rest_framework import mixins
+from rest_framework import generics
 
 # Create your views here.
-class StockList(APIView):
-  """
-    List all stocks, or create a new stock.
-  """
-  def get(self, request, format=None):
-    stocks = Stock.objects.all()
-    serializer = StockSerializer(stocks, many=True)
-    return Response(serializer.data)
+class StockList(mixins.ListModelMixin,
+                mixins.CreateModelMixin,
+                generics.GenericAPIView):
 
-  def post(elf, request, format=None):
-    serializer = StockSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  queryset = Stock.objects.all()
+  serializer_class = StockSerializer
+  
+  def get(self, request, *args, **kwargs):
+    return self.list(request, *args, **kwargs)
+
+  def post(self, request, *args, **kwargs):
+    return self.create(request, *args, **kwargs)
         
 
-class StockDetail(APIView):
-  """
-    Retrieve, update or delete a stock.
-  """
+class StockDetail(mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  generics.GenericAPIView):
 
-  def getStock(self, pk):
-    try:
-        return Stock.objects.get(pk=pk)
-    except Stock.DoesNotExist:
-        raise Http404
-  
-  def get(self, request, pk, format=None):
-    stock = self.getStock(pk)
-    serializer = StockSerializer(stock)
-    return Response(serializer.data)
+  queryset = Stock.objects.all()
+  serializer_class = StockSerializer
 
-  def put(self, request, pk, format=None):
-    stock = self.getStock(pk)
-    serializer = StockSerializer(stock, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  def get(self, request, *args, **kwargs):
+    return self.retrieve(request, *args, **kwargs)
 
-  def delete(self, request, pk, format=None):
-    stock = self.getStock(pk)
-    stock.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+  def put(self, request, *args, **kwargs):
+    return self.update(request, *args, **kwargs)
+    
+  def delete(self, request, *args, **kwargs):
+    return self.destroy(request, *args, **kwargs)
+    
