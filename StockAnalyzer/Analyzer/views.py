4047842818154767dc7a05a884eb256f2e864daf
Daniel Howard <acrodemocide@hotmail.com>
2023-01-25 15:57:40 -0700

    Encapsulated backtest code into function

--- StockAnalyzer/Analyzer/views.py
+++ StockAnalyzer/Analyzer/views.py
@@ -4,6 +4,12 @@ from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+# from PortfolioAnalyzerDriver import return_graph_vals
+
+from importlib.machinery import SourceFileLoader
+PortfolioAnalyzerDriver = SourceFileLoader('PortfolioAnalyzerDriver', '../PortfolioAnalyzerDriver.py').load_module()
+# PortfolioAnalyzerDriver.return_graph_vals()
+# TestModule = SourceFileLoader('test', '../test.py').load_module()
 
 # Create your views here.
 class StockList(generics.ListCreateAPIView):
@@ -20,7 +26,17 @@ class BackTestResults(APIView):
     
     # Create portfolio -- this will return the backtest results
     def post(self, request, format=None):
+        graphValues = PortfolioAnalyzerDriver.return_graph_vals()
+        # PortfolioAnalyzerDriver.testFunc()
+        print(graphValues)
+        # asdf = TestModule.testFunc()
         serializer = StockSerializer(data=request.data)
         if serializer.is_valid():
+            # stock = Stock.objects.create(serializer.data)
+            # stock
+            # stock = serializer.validated_data
+            # stock.businessName += asdf
+            # newSerializer = StockSerializer(stock)
             return Response(serializer.data)
+            # return Response(newSerializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
