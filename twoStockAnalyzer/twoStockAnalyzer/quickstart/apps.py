9c57c4a8e869eee54494d054fdc21808d7ed8830
Loptukqrick <48658100+Loptukqrick@users.noreply.github.com>
2023-02-22 15:10:45 -0700

    Add files via upload

---
+++ twoStockAnalyzer/twoStockAnalyzer/quickstart/apps.py
@@ -0,0 +1,9 @@
+#from django.apps import AppConfig
+
+
+#class QuickstartConfig(AppConfig):
+#    name = 'quickstart'
+#    #name = 'C:\\Users\\chris\\stockAnalyzer1\\stockAnalyzer1'
+#    #default_app_config = 'C:\\Users\\chris\\stockAnalyzer1\\stockAnalyzer1\\quickstart'
+#    #default_auto_field = 'django.db.models.BigAutoField'
+    
