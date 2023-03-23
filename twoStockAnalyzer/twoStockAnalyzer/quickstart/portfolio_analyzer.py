824132a5617feaa627fc0b1c3f041dc20fb65e75
Daniel Howard <acrodemocide@hotmail.com>
2023-03-23 16:42:05 -0600

    Moved all backtest logic back to portfolio_analyzer, leaving only view logic in the view

--- twoStockAnalyzer/twoStockAnalyzer/quickstart/portfolio_analyzer.py
+++ twoStockAnalyzer/twoStockAnalyzer/quickstart/portfolio_analyzer.py
@@ -1,24 +1,198 @@
 import yfinance as yf
 import pandas as pd
 import time
+from .transferObjects.PortfolioResponse import Portfolio
+import time
+import contextlib, io
+
 start_time = time.time()
 
-def backtest():
-    # portfolios = [
-    #     Portfolio(name='aggressive', buy_and_hold_final_value='23458', tactical_rebalance_final_value='6315', 
-    #             buy_and_hold_allocation=[200, 650, 100, 50], tactical_rebalance_allocation=[250, 250, 250, 250], 
-    #             buy_and_hold_graph_data=[], tactical_rebalance_graph_data=[], holdings=['AMRMX', 'AGTHX', 'AMECX', 'ABNDX']),
-    #     Portfolio(name='moderate', buy_and_hold_final_value='18142', tactical_rebalance_final_value='5151', 
-    #             buy_and_hold_allocation=[250, 400, 200, 150], tactical_rebalance_allocation=[250, 250, 250, 250], 
-    #             buy_and_hold_graph_data=[], tactical_rebalance_graph_data=[], holdings=['ANWPX', 'AMECX', 'ABNDX', 'ABALX']),
-    #     Portfolio(name='conservative', buy_and_hold_final_value='11241', tactical_rebalance_final_value='4444', 
-    #             buy_and_hold_allocation=[200, 100, 700], tactical_rebalance_allocation=[250, 250, 250], 
-    #             buy_and_hold_graph_data=[], tactical_rebalance_graph_data=[], holdings=['ABALX', 'AMECX', 'ABNDX']),
-    #     Portfolio(name='index', buy_and_hold_final_value='68686', tactical_rebalance_final_value='5555', 
-    #             buy_and_hold_allocation=[1000], tactical_rebalance_allocation=[1000], 
-    #             buy_and_hold_graph_data=[], tactical_rebalance_graph_data=[], holdings=['VTSMX'])
-    # ]
-    print('Not yet implemented')
+def generate_portfolios():
+    aggressive_holdings = ['AMRMX', 'AGTHX', 'AMECX', 'ABNDX']
+    moderate_holdings = [['ANWPX', 'AMECX', 'ABNDX', 'ABALX']]
+    conservative_holdings = ['ABALX', 'AMECX', 'ABNDX']
+    index_holdings = ['VTSMX']
+
+    portfolios = [
+        Portfolio(
+            name='aggressive',
+            buy_and_hold_final_value='23458',
+            tactical_rebalance_final_value='6315',
+            buy_and_hold_allocation=[200, 650, 100, 50],
+            tactical_rebalance_allocation=[250, 250, 250, 250],
+            buy_and_hold_graph_data=[],
+            tactical_rebalance_graph_data=[],
+            holdings=aggressive_holdings),
+
+        Portfolio(
+            name='moderate',
+            buy_and_hold_final_value='18142',
+            tactical_rebalance_final_value='5151',
+            buy_and_hold_allocation=[250, 400, 200, 150],
+            tactical_rebalance_allocation=[250, 250, 250, 250],
+            buy_and_hold_graph_data=[],
+            tactical_rebalance_graph_data=[],
+            holdings=moderate_holdings),
+
+        Portfolio(
+            name='conservative',
+            buy_and_hold_final_value='11241',
+            tactical_rebalance_final_value='4444',
+            buy_and_hold_allocation=[200, 100, 700],
+            tactical_rebalance_allocation=[250, 250, 250],
+            buy_and_hold_graph_data=[],
+            tactical_rebalance_graph_data=[],
+            holdings=conservative_holdings),
+
+        Portfolio(
+            name='index',
+            buy_and_hold_final_value='68686',
+            tactical_rebalance_final_value='5555',
+            buy_and_hold_allocation=[1000],
+            tactical_rebalance_allocation=[1000],
+            buy_and_hold_graph_data=[],
+            tactical_rebalance_graph_data=[],
+            holdings=index_holdings)
+    ]
+    return portfolios
+
+def backtest(user_portfolio):
+    # TODO: dhoward -- not sure why we're generating these portfolios
+    portfolios = generate_portfolios()
+    
+    ###############################################
+    start_time = time.time()
+    benchmarks_arr = ['AMRMX', 'ANWPX', 'AMECX', 'ABNDX', 'ABALX', 'AGTHX', 'VTSMX']
+    aggressive_list = ['AMRMX', 'AGTHX', 'AMECX', 'ABNDX']
+    moderate_list = ['ANWPX', 'AMECX', 'ABNDX', 'ABALX']
+    conservative_list = ['ABALX', 'AMECX', 'ABNDX']
+    index_list = ['VTSMX']
+
+    user_stock_picks = user_portfolio.holdings
+    # TODO: dhoward -- Do we need the following printout?
+    print('user_stock_picks: ', user_stock_picks)
+    full_list = benchmarks_arr + user_stock_picks
+
+    threaded_list = []
+    import concurrent.futures
+    def main():
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for r in executor.map(extract, full_list): 
+                # ^^ instead of 'full_list' can do 'user_input_arr'
+                # if we create a database to minimize function calls.
+                threaded_list.append(r)
+
+    try:
+        str_io = io.StringIO()
+        with contextlib.redirect_stdout(str_io):
+            main()
+        output = str_io.getvalue()
+
+        if output:
+            # TODO: dhoward -- we need to throw 400 here depending
+            # deppending on the context of the error (i.e.: if we get
+            # an error indicating that a given stock cannot be found)
+            print('Error from yahoo, quit the program')
+            print('yahoo error message: ', output)
+            exit()
+        else:
+            print('no error from yahoo api, continue execution')
+
+    finally:
+        pass
+
+    t1 = clean_and_combine(threaded_list, full_list)
+    aggressive_table = t1.iloc[:,[0,5,2,3]]
+    moderate_table = t1.iloc[:,[1,2,3,4]]
+    conservative_table = t1.iloc[:,[4,2,3]]
+    index_table = t1.iloc[:,[6]]
+    return_table = t1.iloc[:,7:]
+
+
+    period = 20 #21 is roughly monthly, period 3 is roughly weekly
+    percent_table = make_return_percentages(period, user_stock_picks, return_table)
+    aggressive_percent = make_return_percentages(period, aggressive_list, aggressive_table)
+    moderate_percent = make_return_percentages(period, moderate_list, moderate_table)
+    conservative_percent = make_return_percentages(period, conservative_list, conservative_table)
+    index_percent = make_return_percentages(period, index_list, index_table)
+
+
+    # This is repeated... 
+    aggressive_allocation = portfolios[0].buy_and_hold_allocation[:]
+    moderate_allocation = portfolios[1].buy_and_hold_allocation[:]
+    conservative_allocation = portfolios[2].buy_and_hold_allocation[:]
+    index_allocation = portfolios[3].buy_and_hold_allocation[:]
+    buy_and_hold_weightings = user_portfolio.buy_and_hold_allocation[:]
+
+    # Creating Porfolio Objects
+    custom_portfolio = Investment_Portfolio(user_stock_picks)
+    aggressive_portfolio = Investment_Portfolio(aggressive_list)
+    moderate_portfolio = Investment_Portfolio(moderate_list)
+    conservative_portfolio = Investment_Portfolio(conservative_list)
+    index_portfolio = Investment_Portfolio(index_list)
+
+    # Now that the objects exist, we can call the member data from the porfolio object
+    # and perform their own calculations on themselves.
+    buy_and_hold_result = custom_portfolio.buy_and_hold(buy_and_hold_weightings, percent_table)
+    bhagg = aggressive_portfolio.buy_and_hold(aggressive_allocation, aggressive_percent)
+    bhmod = moderate_portfolio.buy_and_hold(moderate_allocation, moderate_percent)
+    bhcon = conservative_portfolio.buy_and_hold(conservative_allocation, conservative_percent)
+    bhind = index_portfolio.buy_and_hold(index_allocation, index_percent)
+
+
+    # rebalance strategy has a bit of extra code to account for the 
+    # way computers store memory. I need to recalculate the portfolio
+    # with every rebalance, but sinces separate variables can refer
+    # to the same memory location, I had to add a few extra things here..
+    # need to pass in weightings for calculations so weightings stay the same
+
+    aggressive_allocation = portfolios[0].tactical_rebalance_allocation[:]
+    moderate_allocation = portfolios[1].tactical_rebalance_allocation[:]
+    conservative_allocation = portfolios[2].tactical_rebalance_allocation[:]
+    index_allocation = portfolios[3].tactical_rebalance_allocation[:]
+    im_port = user_portfolio.tactical_rebalance_allocation[:]
+
+    #############################
+    tactical_rebal_weightings = user_portfolio.tactical_rebalance_allocation[:]
+    im_port = user_portfolio.tactical_rebalance_allocation[:]
+    #############################
+
+    tactical_rebal_result = custom_portfolio.tactical_rebalance(tactical_rebal_weightings, percent_table, im_port)
+
+    ############### --> end calculation <-- ################
+    #### 
+    # Portfolio performance returns:
+    ## standard portfolios for benchmarks ##
+    portfolios[0].buy_and_hold_final_value = bhagg[0]
+    portfolios[1].buy_and_hold_final_value = bhmod[0]
+    portfolios[2].buy_and_hold_final_value = bhcon[0]
+    portfolios[3].buy_and_hold_final_value = bhind[0]
+    portfolios[0].buy_and_hold_allocation = bhagg[1]
+    portfolios[1].buy_and_hold_allocation = bhmod[1]
+    portfolios[2].buy_and_hold_allocation = bhcon[1]
+    portfolios[3].buy_and_hold_allocation = bhind[1]
+    portfolios[0].buy_and_hold_graph_data = bhagg[2]
+    portfolios[1].buy_and_hold_graph_data = bhmod[2]
+    portfolios[2].buy_and_hold_graph_data = bhcon[2]
+    portfolios[3].buy_and_hold_graph_data = bhind[2]
+
+
+    ## custom Portfolio ##
+    updated_data_test = Portfolio(
+        name = 'custom',
+        buy_and_hold_final_value = buy_and_hold_result[0],
+        tactical_rebalance_final_value = tactical_rebal_result[0],
+        buy_and_hold_allocation = buy_and_hold_result[1],
+        tactical_rebalance_allocation = tactical_rebal_result[1],
+        buy_and_hold_graph_data = buy_and_hold_result[2],
+        tactical_rebalance_graph_data = tactical_rebal_result[2],
+        holdings = user_portfolio.holdings
+    )
+
+    print("---%s seconds ---" % (time.time()-start_time))
+    portfolios.append(updated_data_test)
+
+    return portfolios
 
 #def return_graph_vals(user_ticker_list):#, user_ticker_weightings):
 
