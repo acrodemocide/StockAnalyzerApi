20831cf45a9689893564ef3e8b5da1d4e0389bab
Daniel Howard <acrodemocide@hotmail.com>
2024-12-07 09:39:51 -0700

    Updated default return results to empty dict instead of using dummy data

--- services/buy_and_hold.py
+++ services/buy_and_hold.py
@@ -103,39 +103,7 @@ class BuyAndHold(BackTesterInterface):
         #print('date_keys: ', date_keys)
         #print('len(date_keys): ', len(date_keys))
         #"""return dictionary with datetimes and 'snapshots'"""
-        return_dict = {
-                datetime.now(): 100.0,
-                datetime.now() - timedelta(days=1): 200.0,
-                datetime.now() - timedelta(days=2): 300.0,
-                datetime.now() - timedelta(days=3): 400.0,
-                datetime.now() - timedelta(days=4): 500.0,
-                datetime.now() - timedelta(days=5): 600.0,
-                datetime.now() - timedelta(days=6): 700.0,
-                datetime.now() - timedelta(days=7): 800.0,
-                datetime.now() - timedelta(days=8): 900.0,
-                datetime.now() - timedelta(days=9): 1000.0,
-                datetime.now() - timedelta(days=10): 1100.0,
-                datetime.now() - timedelta(days=11): 1200.0,
-                datetime.now() - timedelta(days=12): 1300.0,
-                datetime.now() - timedelta(days=13): 1400.0,
-                datetime.now() - timedelta(days=14): 1500.0,
-                datetime.now() - timedelta(days=15): 1600.0,
-                datetime.now() - timedelta(days=16): 1700.0,
-                datetime.now() - timedelta(days=17): 1800.0,
-                datetime.now() - timedelta(days=18): 1900.0,
-                datetime.now() - timedelta(days=19): 2000.0,
-                datetime.now() - timedelta(days=20): 2100.0,
-                datetime.now() - timedelta(days=21): 2200.0,
-                datetime.now() - timedelta(days=22): 2300.0,
-                datetime.now() - timedelta(days=23): 2400.0,
-                datetime.now() - timedelta(days=24): 2500.0,
-                datetime.now() - timedelta(days=25): 2600.0,
-                datetime.now() - timedelta(days=26): 2700.0,
-                datetime.now() - timedelta(days=27): 2800.0,
-                datetime.now() - timedelta(days=28): 2900.0,
-                datetime.now() - timedelta(days=29): 3000.0,
-                datetime.now() - timedelta(days=30): 3100.0,
-                }
+        return_dict = { }
         if (len(date_keys) > 0):
             return_dict = {date_keys[i].to_pydatetime(): buy_and_hold_custom[2][i] for i in range(len(date_keys))}
         
