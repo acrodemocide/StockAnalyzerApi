ddbcdeda390deea5b54dc3ce8e7ee0f23f3e33bd
Daniel Howard <acrodemocide@hotmail.com>
2024-12-15 13:01:51 -0700

    Updated serializer, view, and algorithms to take initial value for calculating overall portfolio results. Removed commented-out code.

--- api/transfer_objs/portfolio_request.py
+++ api/transfer_objs/portfolio_request.py
@@ -1,6 +1,7 @@
 from typing import Dict
 
 class PortfolioRequest(object):
-    def __init__(self, stocks: Dict[str, float], strategy: str):
+    def __init__(self, stocks: Dict[str, float], strategy: str, initial_value: float):
         self.stocks = stocks
         self.strategy = strategy
+        self.initial_value = initial_value
--- services/back_tester_interface.py
+++ services/back_tester_interface.py
@@ -2,5 +2,5 @@ from typing import Dict
 from api.transfer_objs.portfolio_response import Portfolio
 
 class BackTesterInterface:
-    def backtest(self, stocks: Dict[str, float]) -> Portfolio:
+    def backtest(self, stocks: Dict[str, float], initial_value: float) -> Portfolio:
         pass
